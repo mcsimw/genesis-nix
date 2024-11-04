b9e3a27cb4338241d5bf6d93abb15cd21c1090f5
mcsimw <maor@mcsimw.com>
2024-11-04 13:45:19 -0500

    test 4

--- modules/nix-conf.nix
+++ modules/nix-conf.nix
@@ -2,12 +2,10 @@
 {
   nix.settings = {
     allow-import-from-derivation = false;
-    use-xdg-base-directories = true;
     connect-timeout = 5;
     extra-experimental-features = [
       "nix-command"
       "flakes"
     ];
-    builders-use-substitutes = true;
   };
 }
