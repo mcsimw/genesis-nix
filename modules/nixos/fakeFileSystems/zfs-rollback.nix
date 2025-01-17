2c480d32db5d428ecf9c4c3944e4368da5b140a5
mcsimw <maor@mcsimw.com>
2025-01-17 18:42:24 -0500

    lol

--- modules/nixos/fakeFileSystems/zfs-rollback.nix
+++ modules/nixos/fakeFileSystems/zfs-rollback.nix
@@ -19,7 +19,6 @@ in
         description = ''
           Full description to the volume including pool.
           This volume must have a snapshot to an "empty" state.
-
           WARNING: The volume will be rolled back to the snapshot on every safe-shutdown.
         '';
       };
