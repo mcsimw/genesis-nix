c7fc590af06a6b0b574e72fe467475e06e674c72
mcsimw <maor@mcsimw.com>
2025-01-26 13:17:48 -0500

    test

--- modules/nixos/genesis-iso.nix
+++ modules/nixos/genesis-iso.nix
@@ -1,29 +1,16 @@
 { flake, withSystem, ... }:
-{
-  config,
-  lib,
-  inputs,
-  ...
-}:
+{ config, lib, inputs, ... }:
 let
-  configForSub =
-    {
-      sub,
-      iso ? false,
-    }:
+  configForSub = { sub, iso ? false }:
     let
       modulesPath = "${inputs.nixpkgs.outPath}/nixos";
-
       baseModules = [
         { networking.hostName = sub.hostname; }
         sub.src
-        # Your modules here:
         flake.self.nixosModules.default
         flake.nixos-facter-modules.nixosModules.facter
         flake.self.nixosModules.fakeFileSystems
       ];
-
-      # Extra modules for the ISO variant
       isoModules = [
         {
           imports = [ "${modulesPath}/installer/cd-dvd/installation-cd-minimal-new-kernel.nix" ];
@@ -31,24 +18,19 @@ let
         }
       ];
     in
-    withSystem sub.system (
-      _:
-      flake.nixpkgs.lib.nixosSystem {
-        inherit (sub) system;
-
-        # Pass any special arguments down if needed:
-        specialArgs = withSystem sub.system (
-          { inputs', self', ... }:
-          {
-            inherit (config) packages;
-            inherit self' inputs' inputs;
-          }
-        );
-
-        # If iso=true, we append isoModules.  Otherwise, we append [].
-        modules = baseModules ++ lib.optional iso isoModules;
-      }
-    );
+      withSystem sub.system (_:
+        flake.nixpkgs.lib.nixosSystem {
+          inherit (sub) system;
+          specialArgs = withSystem sub.system (
+            { inputs', self', ... }:
+            {
+              inherit (config) packages;
+              inherit self' inputs' inputs;
+            }
+          );
+          modules = baseModules ++ lib.optionals iso isoModules;
+        }
+      );
 in
 {
   imports = [
@@ -62,53 +44,33 @@ in
             hostname = lib.mkOption {
               type = lib.types.nullOr lib.types.str;
               default = null;
-              description = ''
-                Optional hostname. If null or not set, this submodule is ignored.
-              '';
             };
             src = lib.mkOption {
               type = lib.types.path;
-              description = ''
-                The path to the configuration file or directory for this host.
-              '';
             };
             system = lib.mkOption {
               type = lib.types.str;
               default = "x86_64-linux";
-              description = ''
-                The Nix system architecture (e.g., "x86_64-linux", "aarch64-linux").
-              '';
             };
           };
         }
       );
-      description = "List of host definitions (compootuers).";
-      default = [ ];
+      default = [];
     };
   };
-  config.flake.nixosConfigurations = builtins.listToAttrs (
-    lib.concatMap (
-      sub:
-      if sub.hostname == null then
-        # Skip if no hostname
-        [ ]
-      else
-        [
+  config.flake.nixosConfigurations =
+    builtins.listToAttrs (
+      lib.concatMap (sub:
+        if sub.hostname == null then [] else [
           {
             name = sub.hostname;
-            value = configForSub {
-              inherit sub;
-              iso = false;
-            };
+            value = configForSub { sub = sub; iso = false; };
           }
           {
             name = "${sub.hostname}-iso";
-            value = configForSub {
-              inherit sub;
-              iso = true;
-            };
+            value = configForSub { sub = sub; iso = true; };
           }
         ]
-    ) config.genesis.compootuers
-  );
+      ) config.genesis.compootuers
+    );
 }
