6e0de9b3e4e617920c164d08bf7b93fcaaa67b06
mcsimw <maor@mcsimw.com>
2025-01-27 16:24:56 -0500

    I think I figured it out

--- modules/nixos/genesis.nix
+++ modules/nixos/genesis.nix
@@ -16,9 +16,8 @@ let
       baseModules = [
         { networking.hostName = sub.hostname; }
         sub.src
-        #flake.self.nixosModules.default
-        flake.nixos-facter-modules.nixosModules.facter
-        #flake.self.nixosModules.fakeFileSystems
+        flake.self.nixosModules.default
+        flake.self.nixosModules.fakeFileSystems
       ];
       isoModules = [
         {
