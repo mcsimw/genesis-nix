428857b17245afd623315c62e7d2351bfad2db4c
mcsimw <maor@mcsimw.com>
2025-01-27 09:57:53 -0500

    change

--- modules/packages/default.nix
+++ modules/packages/default.nix
@@ -1,30 +1,23 @@
 { flake, ... }:
-{ config, inputs, lib, ... }:
-
+{
+  config,
+  inputs,
+  ...
+}:
 {
   perSystem =
-    { system, ... }:
-    let
-      pkgs = import inputs.nixpkgs {
+    {
+      pkgs,
+      system,
+      ...
+    }:
+    {
+      _module.args.pkgs = import inputs.nixpkgs {
         inherit system;
         overlays = [
           flake.emacs-overlay.overlays.default
         ];
         config.allowUnfree = true;
       };
-      stage1 = {
-        #        nix = callPackage ./nix-overrides/default.nix { };
-      };
-      finalPackages =
-        (flake.wrapper-manager.lib {
-          pkgs = pkgs // stage1;
-          modules = [
-            ./wrapper-manager/chrome/default.nix
-          ];
-        }).config.build.packages;
-    in
-    {
-      # Flake outputs added by this module:
-      packages = finalPackages;
     };
 }
