0948548b2ae863f7b2eddf839c7a61c943f94659
mcsimw <maor@mcsimw.com>
2025-01-27 11:22:15 -0500

    update

--- modules/nixos/genesis.nix
+++ modules/nixos/genesis.nix
@@ -30,7 +30,7 @@ let
       ];
       nonIsoModules = [
         inputs.nixpkgs.nixosModules.readOnlyPkgs
-        { nixpkgs.pkgs = withSystem system ({ pkgs, ... }: pkgs); }
+        { nixpkgs.pkgs = sub.withSystem system ({ pkgs, ... }: pkgs); }
       ];
     in
     withSystem sub.system (
