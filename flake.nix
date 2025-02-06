d0cd09f151af3a42876383197fe7b76b2faecee1
mcsimw <maor@mcsimw.com>
2025-02-06 10:29:36 -0500

    changes

--- flake.nix
+++ flake.nix
@@ -22,8 +22,8 @@
   };
 
   outputs =
-    inputs@{ flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [
         "x86_64-linux"
         "aarch64-linux"
