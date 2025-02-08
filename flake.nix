c392fb6a543f4cc67155467934740427438a4847
Maor Haimovitz <maor@mcsimw.com>
2025-02-08 09:13:26 -0500

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -15,6 +15,7 @@
     emacs-overlay.url = "github:nix-community/emacs-overlay";
     impermanence.url = "github:nix-community/impermanence";
     chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
+    lix-module = { url = "git+https://git.lix.systems/lix-project/nixos-module.git"; inputs.nixpkgs.follows = "nixpkgs"; inputs.lix.follows = "lix"; };
   };
 
   outputs =
