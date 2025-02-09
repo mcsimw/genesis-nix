0a629f556b9615695f6251489d1f3bc498a22999
mcsimw <maor@mcsimw.com>
2025-02-09 09:52:25 -0500

    Test

--- modules/nixos/compootuers.nix
+++ modules/nixos/compootuers.nix
@@ -22,7 +22,7 @@ let
             config.allowUnfree = true;
             hostPlatform = sub.system;
             overlays = with flake; [
-            #  emacs-overlay.ovelays.default
+              emacs-overlay.ovelays.default
               chaotic.overlays.default
             ];
           };
