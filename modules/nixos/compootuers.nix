022b8de59ea77b87a375961e5369c046a66f9129
mcsimw <maor@mcsimw.com>
2025-02-09 15:59:29 -0500

    test

--- modules/nixos/compootuers.nix
+++ modules/nixos/compootuers.nix
@@ -8,6 +8,7 @@
 }:
 let
   modulesPath = "${inputs.nixpkgs.outPath}/nixos/modules";
+
   configForSub =
     {
       sub,
@@ -17,18 +18,11 @@ let
       baseModules = [
         {
           networking.hostName = sub.hostname;
-          nix.settings = {
-            substituters = [
-              "https://nix-community.cachix.org"
-            ];
-            trusted-public-keys = [
-              "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-            ];
-          };
         }
         flake.self.nixosModules.sane
         flake.self.nixosModules.nix-conf
       ] ++ lib.optionals (sub.both != null) [ sub.both ];
+
       isoModules = [
         {
           imports = [ "${modulesPath}/installer/cd-dvd/installation-cd-base.nix" ];
@@ -43,7 +37,7 @@ let
           };
           users.users.nixos = {
             initialPassword = "iso";
-            /*
+             /*
                For some reason the installation-cd-base.nix sets these two to "", causing a warning
                and potentially stopping my initialPassword setting from working.
             */
@@ -52,31 +46,30 @@ let
           };
         }
       ] ++ lib.optionals (sub.iso != null) [ sub.iso ];
+
       nonIsoModules = [
         flake.self.nixosModules.fakeFileSystems
       ] ++ lib.optionals (sub.src != null) [ sub.src ];
     in
     withSystem sub.system (
-      _:
+      {
+        config,
+        inputs',
+        self',
+        system,
+        ...
+      }:
       inputs.nixpkgs.lib.nixosSystem {
-        specialArgs = withSystem sub.system (
-          {
-            inputs',
-            self',
-            system,
-            ...
-          }:
-          {
-            inherit
-              self'
-              inputs'
-              inputs
-              system
-              ;
-            withSystemArch = withSystem system;
-            packages = config.packages;
-          }
-        );
+        specialArgs = {
+          inherit (config) packages;
+          inherit
+            inputs
+            inputs'
+            self'
+            system
+            ;
+          withSystemArch = withSystem system;
+        };
         modules = baseModules ++ lib.optionals iso isoModules ++ lib.optionals (!iso) nonIsoModules;
       }
     );
@@ -111,6 +104,7 @@ in
     );
     default = [ ];
   };
+
   config.flake.nixosConfigurations = builtins.listToAttrs (
     lib.concatMap (
       sub:
