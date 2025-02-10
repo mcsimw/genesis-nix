445ceabe039ea9ca9dfd6296979a9b78f68eef21
mcsimw <maor@mcsimw.com>
2025-02-10 13:04:16 -0500

    yes

---
+++ modules/nixos/test1.nix
@@ -0,0 +1,136 @@
+{ flake, ... }:
+{
+  config,
+  lib,
+  inputs,
+  withSystem,
+  ...
+}:
+let
+  modulesPath = "${inputs.nixpkgs.outPath}/nixos/modules";
+
+  # Compute the list of compooteurs from the directory structure.
+  compootuersPath =
+    if config.compootuers.path != null
+    then builtins.toString config.compootuers.path
+    else "";
+  computedCompootuers =
+    if config.compootuers.path != null then
+      builtins.concatLists (
+        map (arch:
+          let
+            archPath = compootuersPath + "/" + arch;
+            hostNames = builtins.attrNames (builtins.readDir archPath);
+          in
+            map (host: {
+              hostname = host;
+              system = arch;
+              src = builtins.toPath (archPath + "/" + host);
+            }) hostNames
+        ) (builtins.attrNames (builtins.readDir compootuersPath))
+      )
+    else
+      [];
+
+  configForSub = { sub, iso ? false, }:
+    withSystem sub.system (
+      {
+        config,
+        inputs',
+        self',
+        system,
+        ...
+      }:
+      let
+        baseModules =
+          [
+            { networking.hostName = sub.hostname; }
+            flake.self.nixosModules.sane
+            flake.self.nixosModules.nix-conf
+          ]
+          ++ lib.optional (sub.src != null &&
+                          builtins.pathExists (builtins.toString sub.src + "/both.nix"))
+               (import (builtins.toString sub.src + "/both.nix"));
+        isoModules =
+          [
+            {
+              imports = [ "${modulesPath}/installer/cd-dvd/installation-cd-base.nix" ];
+              boot.initrd.systemd.enable = lib.mkForce false;
+              isoImage.squashfsCompression = "lz4";
+              networking.wireless.enable = lib.mkForce false;
+              systemd.targets = {
+                sleep.enable = lib.mkForce false;
+                suspend.enable = lib.mkForce false;
+                hibernate.enable = lib.mkForce false;
+                hybrid-sleep.enable = lib.mkForce false;
+              };
+              users.users.nixos = {
+                initialPassword = "iso";
+                hashedPasswordFile = null;
+                hashedPassword = null;
+              };
+            }
+          ]
+          ++ lib.optional (sub.src != null &&
+                          builtins.pathExists (builtins.toString sub.src + "/iso.nix"))
+               (import (builtins.toString sub.src + "/iso.nix"));
+        nonIsoModules =
+          [
+            flake.self.nixosModules.fakeFileSystems
+          ]
+          ++ lib.optional (sub.src != null &&
+                          builtins.pathExists (builtins.toString sub.src + "/default.nix"))
+               (import (builtins.toString sub.src + "/default.nix"));
+      in
+      inputs.nixpkgs.lib.nixosSystem {
+        specialArgs = {
+          inherit (config) packages;
+          inherit inputs inputs' self' system;
+          withSystemArch = withSystem system;
+        };
+        modules = baseModules
+          ++ lib.optionals iso isoModules
+          ++ lib.optionals (!iso) nonIsoModules;
+      }
+    );
+
+in
+{
+  options.compootuers = lib.mkOption {
+    type = lib.types.attrs {
+      path = lib.mkOption {
+        type = lib.types.path;
+        default = null;
+      };
+    };
+    default = { path = null; };
+  };
+
+  config = {
+    flake = {
+      nixosConfigurations = builtins.listToAttrs (
+        builtins.concatLists (
+          lib.concatMap (
+            sub:
+            lib.optional (sub.hostname != null)
+              [
+                {
+                  name = sub.hostname;
+                  value = configForSub { inherit sub; iso = false; };
+                }
+                {
+                  name = "${sub.hostname}-iso";
+                  value = configForSub { inherit sub; iso = true; };
+                }
+              ]
+          ) computedCompootuers
+        )
+      );
+    };
+    systems = lib.unique (
+      builtins.filter (s: s != null)
+        (map (sub: sub.system) computedCompootuers)
+    );
+  };
+}
+
