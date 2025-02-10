aed3cda2937ef957922a63c39058d1dc61f88a66
mcsimw <maor@mcsimw.com>
2025-02-10 08:40:41 -0500

    Test

--- modules/nixos/test.nix
+++ modules/nixos/test.nix
@@ -9,77 +9,112 @@
 let
   modulesPath = "${inputs.nixpkgs.outPath}/nixos/modules";
 
-  # Given a base path, return a set with our standardized module paths.
-  completeSrc = base: {
-    iso    = builtins.toPath "${builtins.toString base}/iso.nix";
-    nonIso = builtins.toPath "${builtins.toString base}/non-iso.nix";
-    both   = builtins.toPath "${builtins.toString base}/both.nix";
-  };
-
   configForSub =
-    { sub, iso ? false }:
+    { sub, iso ? false, }:
     let
-      # Base modules are common to both ISO and non‑ISO configurations.
-      # They include the hostname, some shared modules, and then the “both”
-      # module. If the user provided an explicit “both” module, use it;
-      # otherwise, if they provided a base directory in `src`, use the file at <src>/both.nix.
       baseModules =
-        [ {
-            networking.hostName = sub.hostname;
-          }
-        , flake.self.nixosModules.sane
-        , flake.self.nixosModules.nix-conf
-        ]
-        ++ (if sub.both != null then [ sub.both ]
-            else if sub.src != null then [ (completeSrc sub.src).both ]
-            else []);
+        if sub.src != null then
+          [
+            {
+              networking.hostName = sub.hostname;
+            }
+            flake.self.nixosModules.sane
+            flake.self.nixosModules.nix-conf
+          ]
+          ++ (if builtins.pathExists (builtins.toString sub.src + "/both.nix")
+              then [ (import (builtins.toString sub.src + "/both.nix")) ]
+              else [])
+        else
+          [
+            {
+              networking.hostName = sub.hostname;
+            }
+            flake.self.nixosModules.sane
+            flake.self.nixosModules.nix-conf
+          ]
+          ++ lib.optionals (sub.both != null) [ sub.both ];
 
-      # ISO‐specific modules: these include the standard installer modules and
-      # disable certain features. Again, if the user provided an explicit iso module,
-      # that takes precedence; otherwise, if a base directory was given, use <src>/iso.nix.
       isoModules =
-        [ {
-            imports = [ "${modulesPath}/installer/cd-dvd/installation-cd-base.nix" ];
-            boot.initrd.systemd.enable = lib.mkForce false;
-            isoImage.squashfsCompression = "lz4";
-            networking.wireless.enable = lib.mkForce false;
-            systemd.targets = {
-              sleep.enable = lib.mkForce false;
-              suspend.enable = lib.mkForce false;
-              hibernate.enable = lib.mkForce false;
-              hybrid-sleep.enable = lib.mkForce false;
-            };
-            users.users.nixos = {
-              initialPassword = "iso";
-              # The installer module sometimes overrides these values.
-              hashedPasswordFile = null;
-              hashedPassword = null;
-            };
-          }
-        ]
-        ++ (if sub.iso != null then [ sub.iso ]
-            else if sub.src != null then [ (completeSrc sub.src).iso ]
-            else []);
+        if sub.src != null then
+          [
+            {
+              imports = [ "${modulesPath}/installer/cd-dvd/installation-cd-base.nix" ];
+              boot.initrd.systemd.enable = lib.mkForce false;
+              isoImage.squashfsCompression = "lz4";
+              networking.wireless.enable = lib.mkForce false;
+              systemd.targets = {
+                sleep.enable = lib.mkForce false;
+                suspend.enable = lib.mkForce false;
+                hibernate.enable = lib.mkForce false;
+                hybrid-sleep.enable = lib.mkForce false;
+              };
+              users.users.nixos = {
+                initialPassword = "iso";
+                hashedPasswordFile = null;
+                hashedPassword = null;
+              };
+            }
+          ]
+          ++ (if builtins.pathExists (builtins.toString sub.src + "/iso.nix")
+              then [ (import (builtins.toString sub.src + "/iso.nix")) ]
+              else [])
+        else
+          [
+            {
+              imports = [ "${modulesPath}/installer/cd-dvd/installation-cd-base.nix" ];
+              boot.initrd.systemd.enable = lib.mkForce false;
+              isoImage.squashfsCompression = "lz4";
+              networking.wireless.enable = lib.mkForce false;
+              systemd.targets = {
+                sleep.enable = lib.mkForce false;
+                suspend.enable = lib.mkForce false;
+                hibernate.enable = lib.mkForce false;
+                hybrid-sleep.enable = lib.mkForce false;
+              };
+              users.users.nixos = {
+                initialPassword = "iso";
+                hashedPasswordFile = null;
+                hashedPassword = null;
+              };
+            }
+          ]
+          ++ lib.optionals (sub.iso != null) [ sub.iso ];
 
-      # Non‑ISO modules: for example, here we add a fake file systems module.
-      # If a base directory is provided, we also include the file <src>/non-iso.nix.
       nonIsoModules =
-        [ flake.self.nixosModules.fakeFileSystems ]
-        ++ (if sub.src != null then [ (completeSrc sub.src).nonIso ] else []);
-
+        if sub.src != null then
+          [
+            flake.self.nixosModules.fakeFileSystems
+          ]
+          ++ (if builtins.pathExists (builtins.toString sub.src + "/default.nix")
+              then [ (import (builtins.toString sub.src + "/default.nix")) ]
+              else [])
+        else
+          [
+            flake.self.nixosModules.fakeFileSystems
+          ]
+          ++ lib.optionals (sub.src != null) [ sub.src ];
     in
-    withSystem sub.system { config, inputs', self', system, ... }:
+    withSystem sub.system (
+      {
+        config,
+        inputs',
+        self',
+        system,
+        ...
+      }:
       inputs.nixpkgs.lib.nixosSystem {
         specialArgs = {
           inherit (config) packages;
-          inherit inputs inputs' self' system;
+          inherit
+            inputs
+            inputs'
+            self'
+            system;
           withSystemArch = withSystem system;
         };
-        modules =
-          baseModules
-          ++ (if iso then isoModules else nonIsoModules);
-      };
-
+        modules = baseModules ++ lib.optionals iso isoModules ++ lib.optionals (!iso) nonIsoModules;
+      }
+    );
 in
 {
   options.compootuers = lib.mkOption {
@@ -99,8 +134,6 @@ in
             default = null;
           };
           src = lib.mkOption {
-            # The user can provide a base directory (e.g. ./eldritch)
-            # that is automatically completed to iso.nix, non-iso.nix and both.nix.
             type = lib.types.path;
             default = null;
           };
@@ -113,19 +146,28 @@ in
     );
     default = [ ];
   };
-
   config.flake.nixosConfigurations = builtins.listToAttrs (
-    lib.concatMap (sub:
-      if sub.hostname == null then [] else [
-        {
-          name = sub.hostname;
-          value = configForSub { inherit sub; iso = false; };
-        }
-        {
-          name = "${sub.hostname}-iso";
-          value = configForSub { inherit sub; iso = true; };
-        }
-      ]
+    lib.concatMap (
+      sub:
+      if sub.hostname == null then
+        [ ]
+      else
+        [
+          {
+            name = sub.hostname;
+            value = configForSub {
+              inherit sub;
+              iso = false;
+            };
+          }
+          {
+            name = "${sub.hostname}-iso";
+            value = configForSub {
+              inherit sub;
+              iso = true;
+            };
+          }
+        ]
     ) config.compootuers
   );
 }
