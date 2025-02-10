8b24afddaa538063600e4a79c53b25997936a058
mcsimw <maor@mcsimw.com>
2025-02-10 09:06:12 -0500

    yes

--- modules/default.nix
+++ modules/default.nix
@@ -17,6 +17,9 @@ in
     test1 = importApply ./nixos/test1.nix {
       flake = inputs;
     };
+    test2 = importApply ./nixos/test1.nix {
+      flake = inputs;
+    };
     sane = ./nixos/sane.nix;
     nix-conf = ./nixos/nix-conf.nix;
     fakeFileSystems = importApply ./nixos/fakeFileSystems {
