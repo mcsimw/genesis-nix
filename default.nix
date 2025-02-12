411966bbce7aec7588007772085ae0d81e395d54
mcsimw <maor@mcsimw.com>
2025-02-12 13:21:55 -0500

    bye

--- default.nix
+++
@@ -1,8 +0,0 @@
-{ lib, ... }:
-{
-  flake.lib.dirToAttrs =
-    dir:
-    lib.mapAttrs' (name: _value: lib.nameValuePair (lib.removeSuffix ".nix" name) (dir + "/${name}")) (
-      builtins.readDir dir
-    );
-}
