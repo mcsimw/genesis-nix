f039344d63404141d9b39d5b83450c1c9828ffcd
Maor Haimovitz <maor@mcsimw.com>
2025-02-25 12:44:06 -0500

    Update test.nix

--- test.nix
+++ test.nix
@@ -9,36 +9,39 @@
 }:
 let
   modulesPath = "${inputs.nixpkgs.outPath}/nixos/modules";
-  compootuersPath = lib.optionalString (config.compootuers.path != null) (
-    builtins.toString config.compootuers.path
-  );
+  compootuersPath = lib.optionalString (config.compootuers.path != null)
+    (builtins.toString config.compootuers.path);
+
   globalDefault = lib.optional (
-    compootuersPath != "" && builtins.pathExists "${compootuersPath}/default.nix"
+    compootuersPath != "" &&
+    builtins.pathExists "${compootuersPath}/default.nix"
   ) (import "${compootuersPath}/default.nix");
   globalBoth = lib.optional (
-    compootuersPath != "" && builtins.pathExists "${compootuersPath}/both.nix"
+    compootuersPath != "" &&
+    builtins.pathExists "${compootuersPath}/both.nix"
   ) (import "${compootuersPath}/both.nix");
   globalIso = lib.optional (
-    compootuersPath != "" && builtins.pathExists "${compootuersPath}/iso.nix"
+    compootuersPath != "" &&
+    builtins.pathExists "${compootuersPath}/iso.nix"
   ) (import "${compootuersPath}/iso.nix");
 
   computedCompootuers = lib.optionals (compootuersPath != "") (
     let
       compootuersDir = builtins.readDir compootuersPath;
       systems = builtins.filter (system: compootuersDir.${system}.isDir)
-                (builtins.attrNames compootuersDir);
+                 (builtins.attrNames compootuersDir);
     in builtins.concatLists (
       map (system:
         let
           systemPath = "${compootuersPath}/${system}";
           systemDir = builtins.readDir systemPath;
           hostNames = builtins.filter (host: systemDir.${host}.isDir)
-                     (builtins.attrNames systemDir);
+                      (builtins.attrNames systemDir);
         in
-        map (hostName: {
-          inherit hostName system;
-          src = builtins.toPath "${systemPath}/${hostName}";
-        }) hostNames
+          map (hostName: {
+            inherit hostName system;
+            src = builtins.toPath "${systemPath}/${hostName}";
+          }) hostNames
       ) systems
     )
   );
@@ -47,8 +50,7 @@ let
     { sub, iso ? false, }:
     let
       inherit (sub) system src hostName;
-    in
-    withSystem system (
+    in withSystem system (
       { config, inputs', self', system, ... }:
       let
         baseModules = [
@@ -58,7 +60,9 @@ let
           }
           localFlake.nixosModules.sane
           localFlake.nixosModules.nix-conf
-        ] ++ lib.optional (src != null && builtins.pathExists "${src}/both.nix") (import "${src}/both.nix");
+        ]
+        ++ lib.optional (src != null && builtins.pathExists "${src}/both.nix")
+           (import "${src}/both.nix");
         isoModules = [
           {
             imports = [ "${modulesPath}/installer/cd-dvd/installation-cd-base.nix" ];
@@ -73,60 +77,22 @@ let
             };
             users.users.nixos = {
               initialPassword = "iso";
-              # For some reason the installation-cd-base.nix sets these to "",
-              # which may stop initialPassword from working.
               hashedPasswordFile = null;
               hashedPassword = null;
             };
           }
-        ] ++ lib.optional (src != null && builtins.pathExists "${src}/iso.nix") (import "${src}/iso.nix");
-        nonIsoModules = lib.optional (src != null && builtins.pathExists "${src}/default.nix") (
-          import "${src}/default.nix"
-        );
-      in
-      inputs.nixpkgs.lib.nixosSystem {
-        specialArgs = {
-          inherit (config) packages;
-          inherit inputs inputs' self' self system;
-        };
-        modules =
-          baseModules
-          ++ lib.optionals (globalBoth != null) globalBoth
-          ++ lib.optionals (!iso && (globalDefault != null)) globalDefault
-          ++ lib.optionals (iso && (globalIso != null)) globalIso
-          ++ lib.optionals iso isoModules
-          ++ lib.optionals (!iso) nonIsoModules;
-      }
-    );
-in
-{
-  options.compootuers.path = lib.mkOption {
-    type = lib.types.nullOr lib.types.path;
-    default = null;
-  };
-  config = {
-    flake.nixosConfigurations = builtins.listToAttrs (
-      builtins.concatLists (
-        map (
-          sub:
-          let
-            inherit (sub) hostName;
-          in
-          lib.optionals (hostName != null) [
-            {
-              name = hostName;
-              value = configForSub { inherit sub; iso = false; };
-            }
-            {
-              name = "${hostName}-iso";
-              value = configForSub { inherit sub; iso = true; };
-            }
-          ]
-        ) computedCompootuers
-      )
-    );
-    systems = lib.unique (
-      builtins.filter (s: s != null) (map ({ system, ... }: system) computedCompootuers)
-    );
-  };
-}
+        ]
+        ++ lib.optional (src != null && builtins.pathExists "${src}/iso.nix")
+           (import "${src}/iso.nix");
+        nonIsoModules = lib.optional (src != null && builtins.pathExists "${src}/default.nix")
+          (import "${src}/default.nix");
+      in inputs.nixpkgs.lib.nixosSystem {
+          specialArgs = {
+            inherit (config) packages;
+            inherit inputs inputs' self' self system;
+          };
+          modules =
+            baseModules
+            ++ lib.optionals (globalBoth != null) globalBoth
+            ++ lib.optionals (!iso && (globalDefault != null)) globalDefault
+            ++ lib.optionals 
