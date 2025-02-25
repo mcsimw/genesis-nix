5810f097df83d2122c46e1475815634af29a4920
Maor Haimovitz <maor@mcsimw.com>
2025-02-25 13:02:00 -0500

    Update test.nix

--- test.nix
+++ test.nix
@@ -1,6 +1,5 @@
 { localFlake, ... }:
-{
-  config, lib, inputs, withSystem, self, ... }:
+{ config, lib, inputs, withSystem, self, ... }:
 let
   modulesPath = "${inputs.nixpkgs.outPath}/nixos/modules";
   compootuersPath = lib.optionalString (config.compootuers.path != null)
@@ -21,48 +20,90 @@ let
             (hostName: (builtins.getAttr hostName systemDir).type == "directory")
             (builtins.attrNames systemDir);
         in
-          map (hostName: {
-            inherit hostName system;
-            src = builtins.toPath "${systemPath}/${hostName}";
-          }) hostDirs
+        map (hostName: {
+          inherit hostName system;
+          src = builtins.toPath "${systemPath}/${hostName}";
+        }) hostDirs
       ) systems
     )
   );
-  configForSub =
-    { sub, iso ? false, }:
+  configForSub = { sub, iso ? false }: let
+    inherit (sub) system src hostName;
+  in withSystem system (
+    { config, inputs', self', system, ... }:
     let
-      inherit (sub) system src hostName;
+      baseModules =
+        [ {
+            networking.hostName = hostName;
+            nixpkgs.pkgs = withSystem system ({ pkgs, ... }: pkgs);
+          }
+          localFlake.nixosModules.sane
+          localFlake.nixosModules.nix-conf
+        ]
+        ++ lib.optional (src != null && builtins.pathExists "${src}/both.nix")
+           (import "${src}/both.nix");
+      isoModules =
+        [ {
+            imports = [ "${modulesPath}/installer/cd-dvd/installation-cd-base.nix" ];
+            boot.initrd.systemd.enable = lib.mkForce false;
+            isoImage.squashfsCompression = "lz4";
+            networking.wireless.enable = lib.mkForce false;
+            systemd.targets = {
+              sleep.enable = lib.mkForce false;
+              suspend.enable = lib.mkForce false;
+              hibernate.enable = lib.mkForce false;
+              hybrid-sleep.enable = lib.mkForce false;
+            };
+            users.users.nixos = {
+              initialPassword = "iso";
+              hashedPasswordFile = null;
+              hashedPassword = null;
+            };
+          }
+        ]
+        ++ lib.optional (src != null && builtins.pathExists "${src}/iso.nix")
+           (import "${src}/iso.nix");
+      nonIsoModules = lib.optional (src != null && builtins.pathExists "${src}/default.nix")
+           (import "${src}/default.nix");
     in
-    withSystem system (
-      { config, inputs', self', system, ... }:
-      let
-        baseModules =
-          [ {
-              networking.hostName = hostName;
-              nixpkgs.pkgs = withSystem system ({ pkgs, ... }: pkgs);
-            }
-            localFlake.nixosModules.sane
-            localFlake.nixosModules.nix-conf
-          ]
-          ++ lib.optional (src != null && builtins.pathExists "${src}/both.nix")
-             (import "${src}/both.nix");
-        isoModules =
-          [ {
-              imports = [ "${modulesPath}/installer/cd-dvd/installation-cd-base.nix" ];
-              boot.initrd.systemd.enable = lib.mkForce false;
-              isoImage.squashfsCompression = "lz4";
-              networking.wireless.enable = lib.mkForce false;
-              systemd.targets = {
-                sleep.enable = lib.mkForce false;
-                suspend.enable = lib.mkForce false;
-                hibernate.enable = lib.mkForce false;
-                hybrid-sleep.enable = lib.mkForce false;
-              };
-              users.users.nixos = {
-                initialPassword = "iso";
-                hashedPasswordFile = null;
-                hashedPassword = null;
-              };
-            }
-          ]
-          ++ lib.optional (src != null && builtins.pathEx
+    inputs.nixpkgs.lib.nixosSystem {
+      specialArgs = {
+        inherit (config) packages;
+        inherit inputs inputs' self' self system;
+      };
+      modules = baseModules
+        ++ lib.optionals iso isoModules
+        ++ lib.optionals (!iso) nonIsoModules;
+    }
+  );
+in {
+  options.compootuers.path = lib.mkOption {
+    type = lib.types.nullOr lib.types.path;
+    default = null;
+  };
+  config = {
+    flake.nixosConfigurations = builtins.listToAttrs (
+      builtins.concatLists (
+        map (sub:
+          let
+            inherit (sub) hostName;
+          in
+          lib.optional (hostName != null)
+            [ {
+                name = hostName;
+                value = configForSub { inherit sub; iso = false; };
+              }
+              {
+                name = "${hostName}-iso";
+                value = configForSub { inherit sub; iso = true; };
+              }
+            ]
+        ) computedCompootuers
+      )
+    );
+    systems = lib.unique (
+      builtins.filter (s: s != null)
+        (map ({ system, ... }: system) computedCompootuers)
+    );
+  };
+}
